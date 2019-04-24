269d45c3ff297a1baed061dc8ca99d2eca293852
yixianl <46851177+yixianl@users.noreply.github.com>
2019-04-24 19:50:46 -0400

    Create svm.py

---
+++ trainingsvm/svm.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2 as cv
+import pandas as pd
+
+featureData = pd.read_csv('FeatureVector.txt', sep=" ", header=None, usecols = range(20))
+
+#dividing the data into attributes and labels
+X = featureData #attribute
+#temp = featureData.drop(columns=["20"])
+#y = label #label
+
+label = []
+for i in featureData.index:
+    if i<1200:
+        label.append(1)
+    elif i<2400:
+        label.append(2)
+    elif i<3600:
+        label.append(3)
+
+y = label
+
+#seamlessly divide data into training and testing sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+
+#training the algorithm
+from sklearn.svm import SVC
+svclassifier = SVC(kernel='linear')
+svclassifier.fit(X_train, y_train)
+
+#makeing predictions
+y_pred = svclassifier.predict(X_test)
+
+#evaluate the algorithm
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
